125fbf35ba6fc385f5234f99737de193e753531e
Antonio Santoro <santoroantoniogustavo@gmail.com>
2026-01-31 23:36:44 +0100

    fix bug in extract_invoice: handle missing XML nodes

--- src/extract_invoice.py
+++ src/extract_invoice.py
@@ -35,140 +35,131 @@ def convert_p7m(file):
         # In caso di errore → ritorno il file originale
         return nome_file, p7m_bytes
 
-def read_invoice(nome_file,file):
+def safe_findtext(elem, path, default=None):
+    return elem.findtext(path, default) if elem is not None else default
 
+def read_invoice(nome_file, file):
+    import xml.etree.ElementTree as ET
 
-    # Se sono bytes → decodifica
+    # Decodifica file
     if isinstance(file, bytes):
         xml_string = file.decode("utf-8", errors="replace")
-    # Se è una stringa già decodificata → usa direttamente
     elif isinstance(file, str):
         xml_string = str(file).encode("utf-8", errors="replace").decode("utf-8")
     else:
         raise TypeError("Tipo contenuto XML non supportato")
 
-    # Parse XML
     tree = ET.ElementTree(ET.fromstring(xml_string))
-    
-    rif='Nome File:'+nome_file.split('/')[-1] #se funzia questo va cambiato
-
     root = tree.getroot()
 
     # Header
     header = root.find('.//FatturaElettronicaHeader')
-    cedente = header.find('.//CedentePrestatore')
-    committente = header.find('.//CessionarioCommittente')
+    cedente = header.find('.//CedentePrestatore') if header is not None else None
+    committente = header.find('.//CessionarioCommittente') if header is not None else None
 
-    # Cedente Prestatore
     cedente_data = {
-        'denominazione': cedente.findtext('.//Denominazione', ''),
-        'partita_iva': cedente.findtext('.//IdCodice', ''),
-        'codice_fiscale': cedente.findtext('.//CodiceFiscale', ''),
-        'indirizzo': cedente.findtext('.//Indirizzo', ''),
-        'cap': cedente.findtext('.//CAP', ''),
-        'comune': cedente.findtext('.//Comune', ''),
-        'provincia': cedente.findtext('.//Provincia', ''),
-        'nazione': cedente.findtext('.//Nazione', ''),
-        'telefono': cedente.findtext('.//Telefono', ''),
-        'email': cedente.findtext('.//Email', ''),
+        'denominazione': safe_findtext(cedente, './/Denominazione'),
+        'partita_iva': safe_findtext(cedente, './/IdCodice'),
+        'codice_fiscale': safe_findtext(cedente, './/CodiceFiscale'),
+        'indirizzo': safe_findtext(cedente, './/Indirizzo'),
+        'cap': safe_findtext(cedente, './/CAP'),
+        'comune': safe_findtext(cedente, './/Comune'),
+        'provincia': safe_findtext(cedente, './/Provincia'),
+        'nazione': safe_findtext(cedente, './/Nazione'),
+        'telefono': safe_findtext(cedente, './/Telefono'),
+        'email': safe_findtext(cedente, './/Email'),
     }
 
-    # Cessionario Committente
     committente_data = {
-        'denominazione': committente.findtext('.//Denominazione', ''),
-        'partita_iva': committente.findtext('.//IdCodice', ''),
-        'codice_fiscale': committente.findtext('.//CodiceFiscale', ''),
-        'indirizzo': committente.findtext('.//Indirizzo', ''),
-        'cap': committente.findtext('.//CAP', ''),
-        'comune': committente.findtext('.//Comune', ''),
-        'provincia': committente.findtext('.//Provincia', ''),
-        'nazione': committente.findtext('.//Nazione', ''),
-        'telefono': committente.findtext('.//Telefono', ''),
-        'email': committente.findtext('.//Email', ''),
+        'denominazione': safe_findtext(committente, './/Denominazione'),
+        'partita_iva': safe_findtext(committente, './/IdCodice'),
+        'codice_fiscale': safe_findtext(committente, './/CodiceFiscale'),
+        'indirizzo': safe_findtext(committente, './/Indirizzo'),
+        'cap': safe_findtext(committente, './/CAP'),
+        'comune': safe_findtext(committente, './/Comune'),
+        'provincia': safe_findtext(committente, './/Provincia'),
+        'nazione': safe_findtext(committente, './/Nazione'),
+        'telefono': safe_findtext(committente, './/Telefono'),
+        'email': safe_findtext(committente, './/Email'),
     }
 
     # Corpo Fattura
     body = root.find('.//FatturaElettronicaBody')
-    dati_doc = body.find('.//DatiGeneraliDocumento')
-    dati_contratti = body.findall('.//DatiContratto')
-    dati_beni_servizi = body.find('.//DatiBeniServizi')
-    dati_pagamento = body.find('.//DatiPagamento')
-    allegati = body.findall('.//Allegati')
+    dati_doc = body.find('.//DatiGeneraliDocumento') if body is not None else None
+    dati_contratti = body.findall('.//DatiContratto') if body is not None else []
+    dati_beni_servizi = body.find('.//DatiBeniServizi') if body is not None else None
+    dati_pagamento = body.find('.//DatiPagamento') if body is not None else None
+    allegati = body.findall('.//Allegati') if body is not None else []
 
-    # Dati Documento
     documento = {
-        'tipo_documento': dati_doc.findtext('TipoDocumento', ''),
-        'divisa': dati_doc.findtext('Divisa', ''),
-        'data': dati_doc.findtext('Data', ''),
-        'numero': dati_doc.findtext('Numero', ''),
-        'importo_totale': dati_doc.findtext('ImportoTotaleDocumento', ''),
-        'bollo_virtuale': dati_doc.findtext('.//BolloVirtuale', ''),
-        'importo_bollo': dati_doc.findtext('.//ImportoBollo', ''),
-        'causale': ' '.join([c.text for c in dati_doc.findall('Causale')]),
+        'tipo_documento': safe_findtext(dati_doc, 'TipoDocumento'),
+        'divisa': safe_findtext(dati_doc, 'Divisa'),
+        'data': safe_findtext(dati_doc, 'Data'),
+        'numero': safe_findtext(dati_doc, 'Numero'),
+        'importo_totale': safe_findtext(dati_doc, 'ImportoTotaleDocumento'),
+        'bollo_virtuale': safe_findtext(dati_doc, './/BolloVirtuale'),
+        'importo_bollo': safe_findtext(dati_doc, './/ImportoBollo'),
+        'causale': ' '.join([c.text or "" for c in dati_doc.findall('Causale')]) if dati_doc is not None else '',
     }
 
-    # Contratti
     contratti = []
-    for c in dati_contratti:
+    for c in dati_contratti or []:
         contratti.append({
-            'id_documento': c.findtext('IdDocumento', ''),
-            'codice_commessa': c.findtext('CodiceCommessaConvenzione', ''),
+            'id_documento': safe_findtext(c, 'IdDocumento'),
+            'codice_commessa': safe_findtext(c, 'CodiceCommessaConvenzione'),
         })
 
-    # Dettaglio Linee
     linee = []
-    for dettaglio in dati_beni_servizi.findall('DettaglioLinee'):
-        linee.append({
-            'numero_linea': dettaglio.findtext('NumeroLinea', ''),
-            'codice_articolo': dettaglio.findtext('.//CodiceValore', ''),
-            'descrizione': dettaglio.findtext('Descrizione', ''),
-            'quantita': dettaglio.findtext('Quantita', ''),
-            'unita_misura': dettaglio.findtext('UnitaMisura', ''),
-            'prezzo_unitario': dettaglio.findtext('PrezzoUnitario', ''),
-            'prezzo_totale': dettaglio.findtext('PrezzoTotale', ''),
-            'aliquota_iva': dettaglio.findtext('AliquotaIVA', ''),
-            'natura': dettaglio.findtext('Natura', ''),
-            'sconto_maggiorazione': dettaglio.findtext('Sconto/Maggiorazione', ''),
-            'altri_dati_gestionali': [
-                {
-                    'tipo_dato': adg.findtext('TipoDato', ''),
-                    'riferimento_testo': adg.findtext('RiferimentoTesto', '')
-                }
-                for adg in dettaglio.findall('AltriDatiGestionali')
-            ]
-        })
+    if dati_beni_servizi is not None:
+        for dettaglio in dati_beni_servizi.findall('DettaglioLinee'):
+            linee.append({
+                'numero_linea': safe_findtext(dettaglio, 'NumeroLinea'),
+                'codice_articolo': safe_findtext(dettaglio, './/CodiceValore'),
+                'descrizione': safe_findtext(dettaglio, 'Descrizione'),
+                'quantita': safe_findtext(dettaglio, 'Quantita'),
+                'unita_misura': safe_findtext(dettaglio, 'UnitaMisura'),
+                'prezzo_unitario': safe_findtext(dettaglio, 'PrezzoUnitario'),
+                'prezzo_totale': safe_findtext(dettaglio, 'PrezzoTotale'),
+                'aliquota_iva': safe_findtext(dettaglio, 'AliquotaIVA'),
+                'natura': safe_findtext(dettaglio, 'Natura'),
+                'sconto_maggiorazione': safe_findtext(dettaglio, 'Sconto/Maggiorazione'),
+                'altri_dati_gestionali': [
+                    {
+                        'tipo_dato': safe_findtext(adg, 'TipoDato'),
+                        'riferimento_testo': safe_findtext(adg, 'RiferimentoTesto')
+                    }
+                    for adg in dettaglio.findall('AltriDatiGestionali')
+                ]
+            })
 
-    # Riepilogo IVA
     vat_summary = []
-    for riepilogo in dati_beni_servizi.findall('DatiRiepilogo'):
-        vat_summary.append({
-            'aliquota_iva': riepilogo.findtext('AliquotaIVA', ''),
-            'natura': riepilogo.findtext('Natura', ''),
-            'imponibile_importo': riepilogo.findtext('ImponibileImporto', ''),
-            'imposta': riepilogo.findtext('Imposta', ''),
-            'riferimento_normativo': riepilogo.findtext('RiferimentoNormativo', ''),
-        })
+    if dati_beni_servizi is not None:
+        for riepilogo in dati_beni_servizi.findall('DatiRiepilogo'):
+            vat_summary.append({
+                'aliquota_iva': safe_findtext(riepilogo, 'AliquotaIVA'),
+                'natura': safe_findtext(riepilogo, 'Natura'),
+                'imponibile_importo': safe_findtext(riepilogo, 'ImponibileImporto'),
+                'imposta': safe_findtext(riepilogo, 'Imposta'),
+                'riferimento_normativo': safe_findtext(riepilogo, 'RiferimentoNormativo'),
+            })
 
-    # Pagamento
     payments = {
-        'condizioni_pagamento': dati_pagamento.findtext('CondizioniPagamento', ''),
-        'modalita_pagamento': dati_pagamento.findtext('.//ModalitaPagamento', ''),
-        'data_scadenza': dati_pagamento.findtext('.//DataScadenzaPagamento', ''),
-        'importo_pagamento': dati_pagamento.findtext('.//ImportoPagamento', ''),
-        'iban': dati_pagamento.findtext('.//IBAN', ''),
-        'beneficiario': dati_pagamento.findtext('.//Beneficiario', ''),
+        'condizioni_pagamento': safe_findtext(dati_pagamento, 'CondizioniPagamento'),
+        'modalita_pagamento': safe_findtext(dati_pagamento, './/ModalitaPagamento'),
+        'data_scadenza': safe_findtext(dati_pagamento, './/DataScadenzaPagamento'),
+        'importo_pagamento': safe_findtext(dati_pagamento, './/ImportoPagamento'),
+        'iban': safe_findtext(dati_pagamento, './/IBAN'),
+        'beneficiario': safe_findtext(dati_pagamento, './/Beneficiario'),
     }
 
-    # Allegati
     allegati_data = []
-    for a in allegati:
+    for a in allegati or []:
         allegati_data.append({
-            'nome': a.findtext('NomeAttachment', ''),
-            'formato': a.findtext('FormatoAttachment', ''),
-            'contenuto': a.findtext('Attachment', ''),
+            'nome': safe_findtext(a, 'NomeAttachment'),
+            'formato': safe_findtext(a, 'FormatoAttachment'),
+            'contenuto': safe_findtext(a, 'Attachment'),
         })
 
-    # Risultato finale
     fattura = {
         'cedente': cedente_data,
         'committente': committente_data,
@@ -177,16 +168,8 @@ def read_invoice(nome_file,file):
         'linee': linee,
         'riepilogo_iva': vat_summary,
         'pagamento': payments,
-#        'allegati': allegati_data,
+        # 'allegati': allegati_data,
     }
 
     return fattura
 
-'''
-file="../errors/IT00463660399_5QJAF.xml"
-#file="../invoices/IT05006900962_aivdn.xml"
-nome_file,new_file=convert_p7m(file)
-f=read_invoice(nome_file,new_file)
-
-print(f)
-'''
